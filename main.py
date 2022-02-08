3c9d2c0414dccde20546d7d55851835a5bafd99d
Emeka <mjbanks4real@gmail.com>
2022-02-08 05:38:49 +0000

    rest of the code

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ names.lower()
 
 t = names.count('t')
 r = names.count('r')
-u = names.count
+u = names.count('u')
+e = names.count('e')
+
+add_true = t + r + u + e
+
+l = names.count('l')
+o = names.count('o')
+v = names.count('v')
+e = names.count('e')
+
+add_love = l + o + v + e
+
+add_both = int(str(add_true) + str(add_love))
+
+if add_both < 10 or add_both > 90:
+  print(f'Your score is {add_both}, you go together like coke and mentors')
+elif add_both >= 40 and add_both <= 50:
+  print(f'Your score is {add_both}, you are alright together')
+else:
+  print(f'Your score is {add_both}')
 
 
