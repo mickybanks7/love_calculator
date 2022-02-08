8c1274d1b7fe23cc4a9fc2bbe7033e081096ac26
Emeka <mjbanks4real@gmail.com>
2022-02-08 05:13:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+names = name1 + name2
+names.lower()
+
+t = names.count('t')
+r = names.count('r')
+u = names.count
+
+
